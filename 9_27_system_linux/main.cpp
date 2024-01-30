1a72c79923258f1eeab27962bd1ec707dfc423ae
roman <roman@debian>
2024-01-30 22:39:24 +0100

    PC_Home 1/30/2024

---
+++ 9_27_system_linux/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+int main()
+{
+    std::string kommando = "ls -l";
+    std::system(kommando.c_str());
+    kommando.at(6) = 'a';
+    std::system(kommando.c_str());
+}
