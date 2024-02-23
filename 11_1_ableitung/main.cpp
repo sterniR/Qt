fc05c00b1edc50d491bc32e2f5c8065470849278
sterniR <gagamehl200@gmail.com>
2024-02-23 14:58:39 +0100

    2/23/2024

---
+++ 11_1_ableitung/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+class figur
+{
+private:
+    double x, y;
+    std::string farbe;
+public:
+    void werteZuweisen(const double&, const double&, const std::string&);
+    void verschieben(const double&, const double&);
+    void faerben(const std::string&);
+    void ausgeben();
+};
+
+void figur::werteZuweisen(const double& xPos, const double& yPos, const std::string& f)
+{
+    x = xPos;
+    y = yPos;
+    farbe = f;
+}
+
+void figur::verschieben(const double& xDelta, const double& yDelta)
+{
+    x += xDelta;
+    y += yDelta;
+}
+
+void figur::faerben(const std::string& f)
+{
+    farbe = f;
+}
+
+void figur::ausgeben()
+{
+    std::cout << "Figur: " << x << " / " << y << " " << farbe;
+}
+
+class rechteck:public figur
+{
+private:
+    double hoehe, breite;
+private:
+    void werteZuweisen(const double&, const double&, const std::string&, const double&, const double&);
+    void skalieren(const double&, const double&);
+    double flaeche();
+    void ausgeben();
+};
+
+void rechteck::werteZuweisen(const double& xPos, const double& yPos, const std::string& f, const double& h, const double& b)
+{
+    figur::werteZuweisen()
+}
