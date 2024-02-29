2b9b54dbfb1cebfada147f6134faf3b824329bb7
sterniR <gagamehl200@gmail.com>
2024-02-29 09:39:56 +0100

    IT-Bereich 2/29/2024

--- 11_1_ableitung/main.cpp
+++ 11_1_ableitung/main.cpp
@@ -41,7 +41,7 @@ class rechteck:public figur
 {
 private:
     double hoehe, breite;
-private:
+public:
     void werteZuweisen(const double&, const double&, const std::string&, const double&, const double&);
     void skalieren(const double&, const double&);
     double flaeche();
@@ -70,4 +70,65 @@ void rechteck::ausgeben()
 {
     figur::ausgeben();
     std::cout << " Rechteck: " << hoehe << " / " << breite << '\n';
+}
+
+class kreis:public figur
+{
+private:
+    double radius;
+public:
+    void werteZuweisen(const double&, const double&, const std::string&, const double&);
+    void skalieren(const double&);
+    double flaeche();
+    void ausgeben();
+};
+
+void kreis::werteZuweisen(const double& xPos, const double& yPos, const std::string& f, const double& r)
+{
+    figur::werteZuweisen(xPos, yPos, f); //geerbt
+    radius = r;
+}
+
+void kreis::skalieren(const double& radiusFaktor)
+{
+    radius *= radiusFaktor;
+}
+
+double kreis::flaeche()
+{
+    return 4 * atan(1.0) * radius * radius;
+}
+
+void kreis::ausgeben()
+{
+    figur::ausgeben();
+    std::cout << " Kreis: " << radius << '\n';
+}
+
+int main()
+{
+    //Rechteck
+    rechteck ra;
+    ra.werteZuweisen(7.4, 2.3, "Blau", 4.0, 2.6);
+    ra.ausgeben();
+
+    ra.skalieren(0.5, 3.0);
+    ra.verschieben(2.8, 4.2);
+    ra.faerben("Schwarz");
+    ra.ausgeben();
+
+    std::cout << "Rechteck, Flaeche: " << ra.flaeche() << '\n' << '\n';
+
+    //Kreis
+    kreis ka;
+    ka.werteZuweisen(4.6, 1.7, "Blau", 2.3);
+    ka.ausgeben();
+
+    ka.skalieren(2.0);
+    ka.verschieben(3.3, 2.4);
+    ka.faerben("Weiss");
+    ka.ausgeben();
+
+    std::cout << "Kreis, Flaeche: " << ka.flaeche() << '\n';
+
 }
