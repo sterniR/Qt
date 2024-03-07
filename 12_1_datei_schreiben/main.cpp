48dcfee819d4a983b44b5f9caf548e142dd79256
sterniR <gagamehl200@gmail.com>
2024-03-07 09:56:50 +0100

    3/7/24

---
+++ 12_1_datei_schreiben/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream textDatei{"/home/roman/Documents/Qt/datei_schreiben.txt", ios::app};
+
+    if(!textDatei)
+    {
+        std::cout << "Dateifehler" << '\n';
+        return 1;
+    }
+
+    int anzahl = 3;
+    double preis = 1.40;
+    std::string bezeichnung = "Apfel, Bio";
+
+    textDatei << anzahl << " " << preis << " " << bezeichnung << '\n';
+    textDatei << "12 0.85 Birne" << '\n';
+}
