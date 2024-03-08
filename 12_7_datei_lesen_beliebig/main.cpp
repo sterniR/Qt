3ab0d6315640736c853940792ac2659419f781fa
sterniR <gagamehl200@gmail.com>
2024-03-08 13:31:11 +0100

    3/8/24 12_7

---
+++ 12_7_datei_lesen_beliebig/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class postenTyp
+{
+private:
+    int anzahl;
+    double preis;
+    std::string bezeichnung;
+    static const int laenge = 34;
+public:
+    postenTyp();
+    void zeileLesenNummer(ifstream&, const int&);
+    void ausgeben();
+};
+
+postenTyp::postenTyp()
+{
+    anzahl = 0;
+    preis = 0;
+    bezeichnung = "unbekannt";
+}
+
+void postenTyp::zeileLesenNummer(ifstream& td, const int& nummer)
+{
+    std::cout << "Position: " << td.tellg() << '\n';
+    td.seekg(nummer * laenge);
+    std::cout << "Position: " << td.tellg() << '\n';
+
+    td >> anzahl >> preis;
+    std::getline(td, bezeichnung);
+    while(bezeichnung.at(0) == ' ')
+        bezeichnung.erase(0,1);
+}
+
+void postenTyp::ausgeben()
+{
+    cout << anzahl << " " << preis << " |" << bezeichnung << endl;
+}
+
+int main()
+{
+    std::ifstream textDatei{"/home/roman/Documents/Qt/datei_schreiben.txt"};
+    if(!textDatei)
+    {
+        std::cout << "Fehler" << '\n';
+        return 1;
+    }
+
+    postenTyp pEins;
+    pEins.zeileLesenNummer(textDatei, 2);
+    pEins.ausgeben();
+
+    pEins.zeileLesenNummer(textDatei, 1);
+    pEins.ausgeben();
+}
